e597105ac17ef58355a0fb27ba6b26d44946ad07
Alya Shafiqah <alyae98@gmail.com>
2023-03-25 17:20:57 +0800

    Publish dev branch

--- src/App.vue
+++ src/App.vue
@@ -7,8 +7,8 @@
   <div class="flex w-full h-screen bg-primary-darker">
     <div class="flex-1 flex flex-wrap items-center justify-center">
       <div class="space-y-2 text-primary-lighter text-center">
-        <div class="text-h1 font-black uppercase">Coming Soon</div>
-        <div>This site is under development. <br/> Thank you for visiting.</div>
+        <div class="text-h1 font-black uppercase">Development Mode</div>
+        <div>This code in under <code>Dev</code> branch.</div>
       </div>
     </div>
   </div>
